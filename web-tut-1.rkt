3ec28b6357daac9d3b11462ad0f5e5839089888a
Alessandro <alessandrocolangelii@gmail.com>
2021-10-24 19:23:41 -0700

    first commit

---
+++ web-tut-1.rkt
@@ -0,0 +1,53 @@
+#lang web-server/insta
+
+(define (start request)
+  (render-blog-page BLOG request))
+
+(define (render-blog-page a-blog request)
+  (response/xexpr
+   `(html (head (title "My Blog"))
+          (body (h1 "My Blog")
+                ,(render-posts a-blog)))))
+
+; render-post: post -> xexpr
+; Consumes a post, produces an xexpr fragment of the post.
+#;(define (render-post a-post)
+  `(div ((class "post"))
+        ,(post-title a-post)
+        (p ,(post-body a-post))))
+
+#;(define (render-post a-post)
+  (list 'div
+        '((class "post"))
+        (post-title a-post)
+        (list 'p (post-body a-post)) ) )
+
+(define (render-post a-post)
+  (cons 'div
+        (cons '((class "post"))
+              (cons (post-title a-post)
+                    (cons (cons 'p (cons (post-body a-post)'()))
+                     '() ) ) ) ) )
+
+; render-posts: blog -> xexpr
+; Consumes a blog, produces an xexpr fragment
+; of all its posts.
+#;(define (render-posts a-blog)
+  `(div ((class "posts"))
+        ,@(map render-post a-blog)))
+
+(define (render-posts a-blog)
+  (cons 'div
+        (cons '((class "posts"))
+              (map render-post a-blog))))
+
+; A blog is a (listof post)
+; and a post is a (post title body)
+(struct post (title body))
+ 
+; BLOG: blog
+; The static blog.
+(define BLOG
+  (list (post "Second Post" "This is a slight acheivment")
+        (post "First Post" "This is hopefully working")
+        (post "Self made post" "Why not")))
